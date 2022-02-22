c291090d7886a8e0358f8a894f38468c5a10dd8d
Nikolett <nikolettdrahun@gmail.com>
2022-02-22 23:23:12 +0200

    Add game hangman

---
+++ hangman/hangman_pic.py
@@ -0,0 +1,58 @@
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+pic_amount = 7
+
+def render_pic(loose_count, word_lengh):
+    percent = loose_count/word_lengh
+    pic_num = round(percent * (pic_amount - 1))
+
+    print(HANGMANPICS[pic_num])
